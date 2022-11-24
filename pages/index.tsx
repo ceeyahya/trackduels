33ac77e975e3f117cd8654d3355d7c00007fa2eb
Yahya <chahineyahya@pm.me>
2022-11-24 15:26:11 +0000

    Forgot to change the loader's color 🤦

--- pages/index.tsx
+++ pages/index.tsx
@@ -73,11 +73,14 @@ function Home() {
 									href='/vote'>
 									{loading ? (
 										<svg
-											className='h-6 w-6 animate-spin'
+											className='h-6 w-6 animate-spin text-white'
 											xmlns='http://www.w3.org/2000/svg'
 											viewBox='0 0 24 24'>
 											<path fill='none' d='M0 0h24v24H0z' />
-											<path d='M18.364 5.636L16.95 7.05A7 7 0 1 0 19 12h2a9 9 0 1 1-2.636-6.364z' />
+											<path
+												d='M18.364 5.636L16.95 7.05A7 7 0 1 0 19 12h2a9 9 0 1 1-2.636-6.364z'
+												fill='currentColor'
+											/>
 										</svg>
 									) : (
 										'Get Started'
@@ -91,11 +94,14 @@ function Home() {
 									href='/leaderboard'>
 									{loading ? (
 										<svg
-											className='h-6 w-6 animate-spin'
+											className='h-6 w-6 animate-spin text-white'
 											xmlns='http://www.w3.org/2000/svg'
 											viewBox='0 0 24 24'>
 											<path fill='none' d='M0 0h24v24H0z' />
-											<path d='M18.364 5.636L16.95 7.05A7 7 0 1 0 19 12h2a9 9 0 1 1-2.636-6.364z' />
+											<path
+												d='M18.364 5.636L16.95 7.05A7 7 0 1 0 19 12h2a9 9 0 1 1-2.636-6.364z'
+												fill='currentColor'
+											/>
 										</svg>
 									) : (
 										'Leaderboard'
